8388d5a29b8ee945c7650bd4141864bea085f20c
Dimpal123-cmd <dimpalsen574@gmail.com>
2025-03-08 12:33:21 +1400

    Medicine_Finder

---
+++ medfinder/p1/app1/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import *
+# Register your models here.
+
+admin.site.register(Logindata)
+admin.site.register(admindata)
+admin.site.register(medicaldata)
+admin.site.register(medicinedata)
